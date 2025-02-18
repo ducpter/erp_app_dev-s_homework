cd8b88e16304eebbe9c7f2c9a074a5afa81aae75
Ducp <126154839+ducpter@users.noreply.github.com>
2025-02-18 07:50:44 -0800

    Add files via upload

---
+++ spring_mvc/src/main/java/com/example/spring_mvc/CourseService.java
@@ -0,0 +1,20 @@
+package com.example.spring_mvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CourseService {
+    List<Course> getAllCourses();
+    void saveCourse(Course course);
+    Course getCourseById(int id);
+
+    void deleteCourseById(int id);
+
+    Page<Course> findPaginated(int pageNum, int pageSize,
+                               String sortField,
+                               String sortDirection);
+}
